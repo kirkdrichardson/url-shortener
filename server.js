f68e8387c079247a285f7ceec79591d7e142a694
kirk <kirk.douglas.richardson@gmail.com>
2018-03-16 23:05:15 -0600

    added random string generator

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const app = express();
 
+class Util {
+  static getRandomBetween(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+
+  static getRandomLetter() {
+    return String.fromCharCode(this.getRandomBetween(97, 122));
+  }
+
+  static getRandomString(length) {
+    let getLetter;
+    let string = '';
+    // loop {{length}} number of times and add either a number or letter to the return string
+    for (let i = 0; i < length; i++) {
+      getLetter = Boolean(this.getRandomBetween(0, 1));
+      if (getLetter) {
+        string += this.getRandomLetter();
+      } else {
+        string += this.getRandomBetween(0, 9);
+      }
+    }
+    return string;
+  }
+}
+
+console.log(Util.getRandomString(7))
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
